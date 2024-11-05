abd639741e35fefd84bf886f535a5b4551061aaf
Raylan Araujo <raylanra@gmail.com>
2024-11-05 16:16:48 -0300

    atualizado

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,9 +1,12 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
+console.log('O número secreto é ' + numeroSecreto)
 let chute = prompt('Escolha um número entre 0 e 10');
 
+//se o chute for igual ao número secreto
 if(numeroSecreto == chute) {
-    console.log('Isso aí! Você descobriu o número secreto (5)');
+    // Template string é a junção de textos com valores de variáveis. Essa é uma das maneiras de fazer isso em JavaScript
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    console.log('Você errou')
+    alert('Você errou :( ')
 }
