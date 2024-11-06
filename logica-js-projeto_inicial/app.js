d0636a95e1c242cd3e8b9c47e05749b2f9f0f6c4
Raylan Araujo <raylanra@gmail.com>
2024-11-06 17:52:00 -0300

    finalizado o projeto

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,8 @@
 alert('Boas vindas ao jogo do número secreto');
 
 //o comando Math.random() gerar um número aleatório entre 0 (incluido) e 1 (excluido). Ao multiplicar por 100 e somar um ao número sorteado vc acaba aumentando o valor do numéro e passa a ser de 0 a 100. Colocando o parseInt você desconsidera todos os números que vem depois da viírgula
-let numeroSecreto = parseInt(Math.random() * 100 + 1); 
+let valorMaximo = prompt('Digite o valor máximo a ser sorteado')
+let numeroSecreto = parseInt(Math.random() * valorMaximo + 1); 
 
 console.log('O número secreto é ' + numeroSecreto);
 let chute;
@@ -9,7 +10,7 @@ let tentativas =1;
 
 //enquanto chute é diferente do Número Secreto
 while (chute != numeroSecreto){
-    chute = prompt('Escolha um número entre 0 e 100');
+    chute = prompt(`Escolha um número entre 0 e ${valorMaximo}`);
 
     //se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
