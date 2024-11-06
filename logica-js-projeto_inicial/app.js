78734e076b6f96aab365128bcaf0b7a8e031c77e
Raylan Araujo <raylanra@gmail.com>
2024-11-06 07:02:51 -0300

    incluido comando while

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,12 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 4;
+let numeroSecreto = 5;
 console.log('O número secreto é ' + numeroSecreto)
-let chute = prompt('Escolha um número entre 0 e 10');
+let chute 
 
-//se o chute for igual ao número secreto
-if(numeroSecreto == chute) {
-    // Template string é a junção de textos com valores de variáveis. Essa é uma das maneiras de fazer isso em JavaScript
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :( ')
+//enquanto chute é diferente do Número Secreto
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 0 e 10');
+
+    //se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        // Template string é a junção de textos com valores de variáveis. Essa é uma das maneiras de fazer isso em JavaScript
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+    } else {
+        if (chute > numeroSecreto){
+            alert(`O número secret é menor que o ${chute}`)
+        } else {
+            alert(`O número secreto é maior que o ${chute}`)
+        }
+    }
+
 }
