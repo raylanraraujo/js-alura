b96382b18342facdb3f614cc81113a5016b29aaa
Raylan Araujo <raylanra@gmail.com>
2024-11-06 07:11:15 -0300

    incluido o contador de tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,8 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log('O número secreto é ' + numeroSecreto)
-let chute 
+console.log('O número secreto é ' + numeroSecreto);
+let chute;
+let tentativas =1;
 
 //enquanto chute é diferente do Número Secreto
 while (chute != numeroSecreto){
@@ -10,13 +11,14 @@ while (chute != numeroSecreto){
     //se o chute for igual ao número secreto
     if (chute == numeroSecreto) {
         // Template string é a junção de textos com valores de variáveis. Essa é uma das maneiras de fazer isso em JavaScript
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
     } else {
         if (chute > numeroSecreto){
-            alert(`O número secret é menor que o ${chute}`)
+            alert(`O número secret é menor que o ${chute}`);
         } else {
-            alert(`O número secreto é maior que o ${chute}`)
+            alert(`O número secreto é maior que o ${chute}`);
         }
     }
-
+    //tentativas = tentativas + 1
+    tentativas++
 }
