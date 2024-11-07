706aa1f7462606d6d071c6c61cfd4b2926420768
Raylan Araujo <raylanra@gmail.com>
2024-11-07 09:24:55 -0300

    criado funcao com retorno de numero aleatorio e definido para a variavel de numero secreto

--- interagindo_com_HTML/projeto_numero_secreto/app.js
+++ interagindo_com_HTML/projeto_numero_secreto/app.js
@@ -1,3 +1,6 @@
+let numeroSecreto = gerarNumeroAleatorio ();
+
+
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Jogo do número secreto';
 
@@ -7,6 +10,7 @@
 
 //criado funcao para evitar que seja escrito várias linhas com comandos parecidos
 //incluido os parâmetros para que quando a função seja chamada com os parâmetros sejam especificados, eles retornem o resultado desejado
+//função COM PARAMETROS mas SEM RETORNO 
 function exibirTextoNaTela(tag , texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -14,13 +18,18 @@ function exibirTextoNaTela(tag , texto) {
 
 //função chamada com parâmetros especificados na tela
 exibirTextoNaTela('h1' , 'Jogo do número secreto');
-exibirTextoNaTela('p' , 'Escolha um número de 0 a 10')
+exibirTextoNaTela('p' , 'Escolha um número de 0 a 10');
 
 
 //a função é responsável por executar alguma ação dentro do nosso programa. Por padrão e por convenção, é ideal determinar que as funções tenham apenas uma responsabilidade.
+//função SEM PARÂMETROS NEM RETORNO
+//incluido a variavel numeroSecreto para exibir o número aleatório que foi gerado
 function verificarChute(){
-    console.log('O botão foi clicado!');
+    console.log(numeroSecreto);
+}
 
-
-
+// criado a função que gera um número aleatório e retorna esse valor (pois sem ele o número é gerado mas nao fica guardado em lugar algum)
+//função SEM PARÂMETRO, mas COM RETORNO
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
