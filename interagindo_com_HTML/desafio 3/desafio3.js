efca2786616c5305467ddceff2933efb8ec68f72
Raylan Araujo <raylanra@gmail.com>
2024-11-08 13:36:32 -0300

    fazendo os desafio

--- interagindo_com_HTML/desafio 3/desafio3.js
+++ interagindo_com_HTML/desafio 3/desafio3.js
@@ -1,52 +1,46 @@
-//calcular o IMC 
-//altura em metros 
-//peso em quilogramas
+function calcularIMC ( peso , altura) {
+    let imc = peso / (altura * altura);
+    return imc
+}
+console.log (calcularIMC(80 , 1.80))
 
-//criar um funcao imc que tenha peso e altura como parametros
+//--------------------------------------------------
 
-function exibirNaTela(tag , texto) {
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto
+function fatorial(numero) {
+    if (numero == 0 || numero == 1) {
+        return 1 
+    }
+    
+    let fatorial = 1;
+    for ( i = 2 ; i <= numero ; i++){
+        fatorial *= i;
+    }
+    return fatorial;
 }
 
-exibirNaTela ('h1', 'Desafio 3');
-exibirNaTela ('.ex1' , 'Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro');
-exibirNaTela ('.ex2' , 'Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.');
-exibirNaTela ( '.ex3' , 'Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.');
-exibirNaTela ( '.ex4' , 'Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.');
-exibirNaTela ( '.ex5' , 'Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.');
-exibirNaTela ( '.ex6' , 'Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.');
+console.log(fatorial(5))
 
+//--------------------------------------------------
+function conversor(dolares) {
+    let cotacaoDolar = 4.80;
+    let reais = dolares * cotacaoDolar;
+    return reais
+}
+console.log(conversor(3).toFixed(2));
+//--------------------------------------------------
 
+function area (altura , largura) {
+    area = altura * largura;
+    return area
+}
+console.log(area(4,3).toFixed(2) + ' m²');
+//--------------------------------------------------
 
-function calcular(a , b ) {
-    let peso = parseFloat(document.querySelector('.imc__input__peso').value);
-    let altura = parseFloat(document.querySelector ( '.imc__input__altura').value);
-    a = peso;
-    b = altura;
-    console.log (`Peso: ${a}`);
-    console.log(`Altura: ${b}`);
-    let imc = a / (b * b);
-    console.log(`O imc calculado é ${imc.toFixed(2)}`);
-    
-    
-    return imc;
+function areaPerimetoro(raio) {
+    let perimetroCirculo = 2 * Math.PI * raio; 
+    let areaCirculo = Math.PI * raio * raio; 
+    return console.log(`O perímetro é: ${perimetroCirculo} m.\nA área é: ${areaCirculo} m²`)
 }
 
 
-
-function fatorial () {
-    let fatorial = parseInt(document.querySelector('.fatorial_numero').value);
-    let c = fatorial;
-    
-    console.log(`O valor do fatorial é ${fatorial}`);
-    console.log(`O valor de c é ${c}`)
-    console.log('ok');
-
-    
-
-    
-    
-}
-console.log(resultadoFatorial)
-
+areaPerimetoro(4);
---
+++ interagindo_com_HTML/desafio 3/rascunhoDesafio.js
@@ -0,0 +1,52 @@
+//calcular o IMC 
+//altura em metros 
+//peso em quilogramas
+
+//criar um funcao imc que tenha peso e altura como parametros
+
+function exibirNaTela(tag , texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto
+}
+
+exibirNaTela ('h1', 'Desafio 3');
+exibirNaTela ('.ex1' , 'Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro');
+exibirNaTela ('.ex2' , 'Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.');
+exibirNaTela ( '.ex3' , 'Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.');
+exibirNaTela ( '.ex4' , 'Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.');
+exibirNaTela ( '.ex5' , 'Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.');
+exibirNaTela ( '.ex6' , 'Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.');
+
+
+
+function calcular(a , b ) {
+    let peso = parseFloat(document.querySelector('.imc__input__peso').value);
+    let altura = parseFloat(document.querySelector ( '.imc__input__altura').value);
+    a = peso;
+    b = altura;
+    console.log (`Peso: ${a}`);
+    console.log(`Altura: ${b}`);
+    let imc = a / (b * b);
+    console.log(`O imc calculado é ${imc.toFixed(2)}`);
+    
+    
+    return imc;
+}
+
+
+
+function fatorial () {
+    let fatorial = parseInt(document.querySelector('.fatorial_numero').value);
+    let c = fatorial;
+    
+    console.log(`O valor do fatorial é ${fatorial}`);
+    console.log(`O valor de c é ${c}`)
+    console.log('ok');  
+}
+console.log(resultadoFatorial)
+
+function Fatorial(numero) {
+    numero *= (numero-1);
+    return res
+}
+console.log(res)
