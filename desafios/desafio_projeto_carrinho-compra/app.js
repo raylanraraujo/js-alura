9acc055b49f728ab9bb711935d241e4ef4bd707b
Raylan Araujo <raylanra@gmail.com>
2024-11-27 09:41:28 -0300

    desafios alura

---
+++ desafios/desafio_projeto_carrinho-compra/app.js
@@ -0,0 +1,13 @@
+console.log('começou')
+
+let duasFrases = 'Penso logo existo;Passarinho que come pedra sabe o cu que tem.';
+let frasesSeparadas = duasFrases.split(';');
+console.log(frasesSeparadas);
+
+
+let numeros = '12, 54, 21,353, 98'
+let numeroSeparado = numeros.split(',');
+console.log(numeroSeparado);
+numeroSeparado.pop();
+console.log(numeroSeparado)
+console.log('terminou')
